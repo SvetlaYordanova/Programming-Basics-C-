10093039179b8d8571cc5331e8c23da3f10c92b8
SvetlaYordanova <32373038+SvetlaYordanova@users.noreply.github.com>
2018-03-06 19:51:48 +0200

    Add files via upload

---
+++ Coding 101 Exam - 6 March 2016/Training Lab/Training Lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Training_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+
+            w = w * 100;
+            h = h * 100;
+
+            double dyljina = Math.Floor(h /120);
+
+            double chirina = Math.Floor((w - 100) / 70);
+
+            double place = (dyljina * chirina);
+
+            Console.WriteLine(place - 3);
+
+        }
+    }
+}
