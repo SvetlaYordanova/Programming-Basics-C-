9eb50c40b19dc8bb4a6a402d3e7233577480d7d7
SvetlaYordanova <32373038+SvetlaYordanova@users.noreply.github.com>
2018-03-06 19:57:19 +0200

    Add files via upload

---
+++ Прости пресмятания/USD to BGN/curenci converter new/curenci converter new/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace curenci_converter_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        decimal sumForConcert = decimal.Parse(Console.ReadLine());
+
+                        string valuta = Console.ReadLine().ToUpper();
+
+                        string valutaTo = Console.ReadLine().ToUpper();
+
+                        decimal sum = 0;
+
+                        decimal bgn = 1.00m;
+                        decimal usd = 1.79549m;
+
+                        decimal eur = 1.95583m;
+                        decimal gbp = 2.53405m;
+
+
+                        switch (valuta)
+                        {
+                            case "BGN":
+                                sum = sumForConcert; break;
+                            case "USD":
+                                sum = sumForConcert * usd; break;
+                            case "EUR":
+                                sum = sumForConcert * eur; break;
+                            case "GBP":
+                                sum = sumForConcert * gbp; break;
+                            default: break;
+
+                        }
+                        switch (valutaTo)
+                        {
+                            case "BGN":
+                                sum = sumForConcert / bgn; break;
+                            case "USD":
+                                sum = sumForConcert / usd; break;
+                            case "EUR":
+
+                                sum = sumForConcert / eur; break;
+                            case "GBP":
+                                sum = sumForConcert / gbp; break;
+                            default: break;
+                        }
+
+                        Console.WriteLine(Math.Round(sum, 2));
+
+                    }
+                }
+            }
+
