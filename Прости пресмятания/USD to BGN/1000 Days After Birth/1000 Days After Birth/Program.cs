82e2d00dfa126a9b6658498aaf7893a10351db21
SvetlaYordanova <32373038+SvetlaYordanova@users.noreply.github.com>
2018-03-06 19:55:27 +0200

    Add files via upload

---
+++ Прости пресмятания/USD to BGN/1000 Days After Birth/1000 Days After Birth/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using global::System;
+using global::System.Globalization;
+
+
+namespace _1000_Days_After_Birth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateStr = System.Console.ReadLine();
+            string format = "dd-MM-yyyy";
+
+            System.DateTime datetime = System.DateTime.ParseExact(dateStr,format, System.Globalization.CultureInfo.InvariantCulture);
+
+            System.DateTime after = datetime.AddDays(999);
+
+            System.Console.WriteLine(after.ToString("dd-MM-yyyy"));
+        }
+    }
+}
---
+++ Прости пресмятания/USD to BGN/2D Rectangle Area/2D Rectangle Area/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2D_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //x1, y1, x2 и y2 
+
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double width = Math.Abs(x1 - x2);
+            double length = Math.Abs(y1 - y2);
+
+            double area = width * length;
+
+            double perimeter = 2 * (width + length);
+
+            Console.WriteLine(area);
+            Console.WriteLine(perimeter);
+
+
+        }
+    }
+}
