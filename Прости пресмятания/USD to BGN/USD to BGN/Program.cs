a2517d0a15519d94bab3de462cd982b237ffcd86
SvetlaYordanova <32373038+SvetlaYordanova@users.noreply.github.com>
2018-03-06 19:54:02 +0200

    Add files via upload

---
+++ Прости пресмятания/USD to BGN/USD to BGN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal usd = decimal.Parse(Console.ReadLine());
+
+            decimal bgn = usd * 1.79549m;
+
+            Console.WriteLine(Math.Round(bgn,2));
+
+            //Console.WriteLine("{0:f2}",bgn);
+
+
+
+        }
+    }
+}
