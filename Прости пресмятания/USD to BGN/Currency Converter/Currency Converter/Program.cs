f5b06268436cc19cb168efb5bd87dd804ab49e70
SvetlaYordanova <32373038+SvetlaYordanova@users.noreply.github.com>
2018-03-06 20:01:21 +0200

    Add files via upload

---
+++ Прости пресмятания/USD to BGN/Currency Converter/Currency Converter/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Currency_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sumForConvert = decimal.Parse(Console.ReadLine());
+
+            string valutaVhod = Console.ReadLine().ToLower();
+
+            string valutaIzhod = Console.ReadLine().ToLower();
+
+            decimal finlaSum = 0;
+
+            //BGN, USD- 1.79549; EUR- 1.95583; GBP-2.53405;
+
+            if (valutaVhod == "bgn")
+            {
+                if (valutaIzhod == "bgn")
+                {
+                    finlaSum = sumForConvert;
+                }
+
+                else if (valutaIzhod == "usd")
+                {
+                    finlaSum = sumForConvert / 1.79549m;
+                }
+                else if (valutaIzhod == "eur")
+                {
+                    finlaSum = sumForConvert / 1.95583m;
+                }
+                else if (valutaIzhod == "gbp")
+                {
+                    finlaSum = sumForConvert / 2.53405m;
+                }
+
+            }
+            if (valutaVhod == "usd")
+            {
+                if (valutaIzhod == "bgn")
+                {
+                    finlaSum = sumForConvert *1.79549m;
+                }
+
+                else if (valutaIzhod == "usd")
+                {
+                    finlaSum = sumForConvert;
+                }
+                else if (valutaIzhod == "eur")
+                {
+                    finlaSum = (sumForConvert * 1.79549m) / 1.95583m;
+                }
+                else if (valutaIzhod == "gbp")
+                {
+                    finlaSum = (sumForConvert * 1.79549m) / 2.53405m;
+                }
+
+            }
+
+            if (valutaVhod == "eur")
+            {
+                if (valutaIzhod == "bgn")
+                {
+                    finlaSum = sumForConvert / 1.95583m;
+                }
+                else if (valutaIzhod == "usd")
+                {
+                    finlaSum = (sumForConvert * 1.95583m) / 1.79549m;
+
+                }
+                else if (valutaIzhod == "eur")
+                {
+                    finlaSum = sumForConvert;
+                }
+
+                else if (valutaIzhod == "gbp")
+                {
+                    finlaSum = (sumForConvert * 1.95583m) / 2.53405m;
+                }
+            }
+
+            if (valutaVhod == "gbp")
+            {
+                if (valutaIzhod == "bgn")
+                {
+                    finlaSum = sumForConvert * 2.53405m;
+                }
+
+                else if (valutaIzhod == "usd")
+                {
+                    finlaSum = (sumForConvert * 2.53405m ) / 1.79549m ;
+                }
+                else if (valutaIzhod == "eur")
+                {
+                    finlaSum = (sumForConvert * 2.53405m ) / 1.95583m;
+                }
+                else if (valutaIzhod == "gbp")
+                {
+                    finlaSum = sumForConvert;
+                }
+            }
+            finlaSum= Math.Round(finlaSum,2);
+
+            Console.WriteLine(finlaSum +" "+ valutaIzhod.ToUpper());
+        }
+
+        }
+        }
+       
+    
+
+
+
---
+++ Прости пресмятания/USD to BGN/Currency Converter2/Currency Converter2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+
+
+namespace Currency_Converter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sumForConcert = decimal.Parse(Console.ReadLine());
+
+            string valuta = Console.ReadLine().ToUpper();
+
+            string valutaTo = Console.ReadLine().ToUpper();
+
+            decimal sum = 0;
+
+            decimal bgn = 1;
+            decimal usd = 1.79549;
+
+            decimal eur = 1.95583;
+            decimal gbp = 2.53405;
+
+
+            switch (valuta)
+            {
+                case "BGN":
+                    sum = sumForConcert; break;
+                case "USD":
+                    sum = sumForConcert * usd; break;
+                case "EUR":
+                    sum = sumForConcert * eur; break;
+                case "GBP":
+                    sum = sumForConcert * gbp; break;
+                    default:break;
+
+            }
+            switch (valutaTo)
+            {
+                case "BGN":
+                    sum = sumForConcert / bgn; break;
+                case "USD":
+                    sum = sumForConcert / usd; break;
+                case "EUR":
+
+                    sum = sumForConcert / eur; break;
+                case "GBP":
+                    sum = sumForConcert / gbp; break;
+                    default:break;
+            }
+
+            Console.WriteLine(Math.Round(sum, 2));
+
+        }
+    }
+}
